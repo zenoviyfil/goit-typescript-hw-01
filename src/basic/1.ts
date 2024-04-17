1262d7df65bf14846151ebcb21dd7f302fde6463
Zenoviy Fil <98259328+zenoviyfil@users.noreply.github.com>
2024-04-17 09:54:39 +0200

    error fix

--- src/basic/1.ts
+++ src/basic/1.ts
@@ -2,4 +2,4 @@ const age: number = 50;
 const nameA:string = 'Max';
 const toggle:boolean = true;
 const empty:null = null;
-const callback = (a:number) => { return 100 + a };
+const callback = (a:number):number => { return 100 + a };
