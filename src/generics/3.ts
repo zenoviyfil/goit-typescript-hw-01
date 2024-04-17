6e7d8ef2095b46a9ffda54a40a2f792a92cff33b
Zenoviy Fil <98259328+zenoviyfil@users.noreply.github.com>
2024-04-17 13:15:07 +0200

    fix file 3

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,4 +1,4 @@
-function merge<T extends {}, U extends {}>(objA: T, objB: U) {
-      return Object.assign(objA, objB);
+function merge<T extends {}, U extends {}>(objA: T, objB: U):(T & U) {
+      return Object.assign({}, objA, objB);
     }
     
